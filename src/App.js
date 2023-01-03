e6d9fa475f2dd2c17e2f2f60025ef18349b6713e
Aleksey Shingarev <alekseytim@mail.ru>
2023-01-03 17:54:40 +0300

    feat: Sidebar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Li
 import './App.css'
 
 const App = () => {
-  const activeMenu = false
+  const activeMenu = true
   return (
     <div>
       <BrowserRouter>
