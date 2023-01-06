1a0442f00090e47fb3203fa7bedb0b22d3772bc9
Aleksey Shingarev <alekseytim@mail.ru>
2023-01-06 12:20:38 +0300

    feat: Data tables

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components'
+import { generateRandomString } from '../utils/generateRandomString'
+
 
 const Customers = () => {
   return (
-    <div>
-
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl '>
+      <Header category='Page' title='Customers'></Header>
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete', 'Add', 'Edit']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item) => (
+            <ColumnDirective key={generateRandomString} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+      </GridComponent>
     </div>
   )
 }
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+import { generateRandomString } from '../utils/generateRandomString'
 
 const Orders = () => {
   return (
-    <div>
-
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl '>
+      <Header category='Page' title='Orders'></Header>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item) => (
+            <ColumnDirective key={generateRandomString} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
     </div>
   )
 }
